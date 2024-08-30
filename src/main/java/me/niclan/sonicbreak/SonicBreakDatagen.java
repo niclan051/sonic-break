55ff3173f0dd396f720508d2ecce35f6db26912b
niclan051 <niclan052@gmail.com>
2024-08-30 10:42:44 +0300

    removed unused import

--- src/main/java/me/niclan/sonicbreak/SonicBreakDatagen.java
+++ src/main/java/me/niclan/sonicbreak/SonicBreakDatagen.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBlockTags;
-import net.minecraft.block.StairsBlock;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryWrapper.WrapperLookup;
 import net.minecraft.registry.tag.BlockTags;
